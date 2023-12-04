b8f68197a10d86060c2866a75b524f577e361662
wadada <w.daiki1175@gmail.com>
2023-12-04 10:51:04 +0900

    12/3

--- sever_blockchain.py
+++ sever_blockchain.py
@@ -6,9 +6,34 @@ import wallet
 app = Flask(__name__)
 
 
-@app.route("/")
-def hello_world():
-    return "Hello, World!"
+cache = {}
+
+
+def get_blockchain():
+    cached_blockchain = cache.get("blockchain")
+    if not cached_blockchain:
+        miners_wallet = wallet.wallet()
+        cache["blockchain"] = blockchain.BlockChain(
+            blockchain_address=miners_wallet.blockchain_address,
+            port=app.config["port"],
+        )
+    app.logger.warning(
+        {
+            "private_key": miners_wallet.private_key,
+            "public_key": miners_wallet.public_key,
+            "blockchain_address": miners_wallet.blockchain_address,
+        }
+    )
+    return cache["blockchain"]
+
+
+@app.route("/chain", methods=["GET"])
+def gety_chain():
+    block_chain = get_blockchain()
+    response = {
+        "chain": block_chain.chain,
+    }
+    return jsonify(response), 200
 
 
 if __name__ == "__main__":
